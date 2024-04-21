87fdc212986dfdece313abacd13cfa450e878705
Alexander Mottram <alexanderdmottram@outlook.com>
2024-04-21 09:59:34 +0100

    Improve equality operation functionality.

--- utils/array_template.h
+++ utils/array_template.h
@@ -5,139 +5,133 @@
 
 #include "precompile_header.h"
 
-namespace utils
-{
+namespace utils {
 
-	template<class T>
-	class Array2D
-	{
-	 public:
-		using VecType = std::vector<T>;
-		using DualVecType = std::vector<VecType>;
+    template<class T>
+    class Array2D {
+    public:
+        using VecType = std::vector<T>;
+        using DualVecType = std::vector<VecType>;
 
-		explicit Array2D(const DualVecType& input_data);
+        explicit Array2D(const DualVecType& input_data);
 
-		template<typename IdxT>
-		T& at(IdxT x, IdxT y)
-		{
-			auto x_long = static_cast<long long> (x);
-			auto y_long = static_cast<long long> (y);
-			return at_cardinal(x_cardinal(x_long), y_cardinal(y_long));
-		}
+        template<typename IdxT>
+        T& at(IdxT x, IdxT y)
+        {
+            auto x_long = static_cast<long long> (x);
+            auto y_long = static_cast<long long> (y);
+            return at_cardinal(x_cardinal(x_long), y_cardinal(y_long));
+        }
 
-		T& at(Vec2D vec)
-		{
-			return at(vec.x, vec.y);
-		}
+        T& at(Vec2D vec)
+        {
+            return at(vec.x, vec.y);
+        }
 
-		[[nodiscard]] size_t index_to_x_cardinal(size_t index) const
-		{
-			return index % size_y;
-		}
+        [[nodiscard]] size_t index_to_x_cardinal(size_t index) const
+        {
+            return index%size_y;
+        }
 
-		[[nodiscard]] long long index_to_actual_x(size_t index) const
-		{
-			return index_to_x_cardinal(index) + x_origin;
-		}
+        [[nodiscard]] long long index_to_actual_x(size_t index) const
+        {
+            return index_to_x_cardinal(index)+x_origin;
+        }
 
-		[[nodiscard]] size_t index_to_y_cardinal(size_t index) const
-		{
-			return index / size_y;
-		}
+        [[nodiscard]] size_t index_to_y_cardinal(size_t index) const
+        {
+            return index/size_y;
+        }
 
-		[[nodiscard]] long long index_to_actual_y(size_t index) const
-		{
-			return index_to_y_cardinal(index) + y_origin;
-		}
+        [[nodiscard]] long long index_to_actual_y(size_t index) const
+        {
+            return index_to_y_cardinal(index)+y_origin;
+        }
 
-		[[nodiscard]] size_t size() const
-		{
-			return this->data.size();
-		}
+        [[nodiscard]] size_t size() const
+        {
+            return this->data.size();
+        }
 
-		[[nodiscard]] size_t get_size_x() const
-		{
-			return size_x;
-		}
+        [[nodiscard]] size_t get_size_x() const
+        {
+            return size_x;
+        }
 
-		[[nodiscard]] size_t get_size_y() const
-		{
-			return size_y;
-		}
+        [[nodiscard]] size_t get_size_y() const
+        {
+            return size_y;
+        }
 
-		friend bool operator<(Array2D<T> const& lhs, Array2D<T> const& rhs)
-		{
-			return (lhs.data < rhs.data);
-		}
+        friend bool operator< <T>(Array2D<T> const& lhs, Array2D<T> const& rhs);
 
-		friend bool operator==(Array2D<T> const& lhs, Array2D<T> const& rhs)
-		{
-			return (lhs.data == rhs.data);
-		}
+        friend bool operator== <T>(Array2D<T> const& lhs, Array2D<T> const& rhs);
 
-		// Standard vector based begin and ends
-		auto begin()
-		{
-			return data.begin();
-		}
+        friend std::ostream& operator<< <T>(
+                std::ostream& os,
+                const Array2D<T>& a
+        );
 
-		auto end()
-		{
-			return data.end();
-		}
+        // Standard vector based begin and ends
+        auto begin()
+        {
+            return data.begin();
+        }
 
-		auto begin() const
-		{
-			return data.begin();
-		}
+        auto end()
+        {
+            return data.end();
+        }
 
-		auto end() const
-		{
-			return data.end();
-		}
+        auto begin() const
+        {
+            return data.begin();
+        }
 
-	 private:
-		VecType data{};
-		size_t size_x{};
-		size_t size_y{};
-		long long x_origin{ 0 };
-		long long y_origin{ 0 };
+        auto end() const
+        {
+            return data.end();
+        }
 
-		size_t x_cardinal(long long x);
+    private:
+        VecType data{};
+        size_t size_x{};
+        size_t size_y{};
+        long long x_origin{0};
+        long long y_origin{0};
 
-		size_t y_cardinal(long long y);
+        size_t x_cardinal(long long x);
 
-		T& at_cardinal(const size_t& x_cardinal, const size_t& y_cardinal)
-		{
-			return data.at(cardinal_to_index(x_cardinal, y_cardinal));
-		}
+        size_t y_cardinal(long long y);
 
-		size_t cardinal_to_index(const size_t& x_cardinal, const size_t& y_cardinal)
-		{
-			return (y_cardinal * size_x) + x_cardinal;
-		}
+        T& at_cardinal(const size_t& x_cardinal, const size_t& y_cardinal)
+        {
+            return data.at(cardinal_to_index(x_cardinal, y_cardinal));
+        }
 
-	};
+        size_t cardinal_to_index(const size_t& x_cardinal, const size_t& y_cardinal)
+        {
+            return (y_cardinal*size_x)+x_cardinal;
+        }
 
-	template<class T>
-	Array2D<T>::Array2D(const DualVecType& input_data)
-	{
-		this->size_y = input_data.size();
-		this->size_x = input_data.front().size();
-		for (const auto& row : input_data)
-		{
-			if (row.size() != this->size_x)
-			{
-				throw std::invalid_argument("Uneven subvector sizes.");
-			}
-			for (auto val : row)
-			{
-				this->data.push_back(val);
-			}
-		}
-	}
+    };
 
-	    template<class T>
+    template<class T>
+    Array2D<T>::Array2D(const DualVecType& input_data)
+    {
+        this->size_y = input_data.size();
+        this->size_x = input_data.front().size();
+        for (const auto& row : input_data) {
+            if (row.size()!=this->size_x) {
+                throw std::invalid_argument("Uneven subvector sizes.");
+            }
+            for (auto val : row) {
+                this->data.push_back(val);
+            }
+        }
+    }
+
+    template<class T>
     size_t Array2D<T>::y_cardinal(long long int y)
     {
         long long y_card = y-y_origin;
@@ -161,31 +155,44 @@ namespace utils
         }
     }
 
-	template<class T>
-	std::ostream& operator<<(std::ostream& os, const Array2D<T>& a)
-	{
-		for (auto i{ 0 }; i < a.data.size(); i++)
-		{
-			// Pre value insert
-			if (i % a.size_x == 0)
-			{
-				os << "[";
-			}
-			// Value insert
-			os << a.data.at(i);
+    template<class T>
+    bool operator<(Array2D<T> const& lhs, Array2D<T> const& rhs)
+    {
+        return (
+                (lhs.size()<rhs.size())
+                || ((lhs.size()==rhs.size()) && (lhs.data<rhs.data))
+                );
+    };
 
-			// Post value insert
-			if ((i + 1) % a.size_x == 0)
-			{
-				os << "]\n";
-			}
-			else
-			{
-				os << ",";
-			}
-		}
+    template<class T>
+    bool operator==(Array2D<T> const& lhs, Array2D<T> const& rhs)
+    {
+        return (lhs.get_size_x()==rhs.get_size_x())
+                && (lhs.get_size_y()==rhs.get_size_y())
+                && (lhs.data==rhs.data);
+    };
 
-		return os;
-	}
+    template<class T>
+    std::ostream& operator<<(std::ostream& os, const Array2D<T>& a)
+    {
+        for (auto i{0}; i<a.data.size(); i++) {
+            // Pre value insert
+            if (i%a.size_x==0) {
+                os << "[";
+            }
+            // Value insert
+            os << a.data.at(i);
+
+            // Post value insert
+            if ((i+1)%a.size_x==0) {
+                os << "]\n";
+            }
+            else {
+                os << ",";
+            }
+        }
+
+        return os;
+    }
 
 }
--- utils_tests/array_template_tests.cpp
+++ utils_tests/array_template_tests.cpp
@@ -1,56 +1,90 @@
 #pragma clang diagnostic ignored "-Woverloaded-shift-op-parentheses"
+
 #include "../doctest.h"
 #include "../utils/utils.h"
 
-struct VectorsAndInt
-{
-	int x;
-	int y;
-	int val;
+struct VectorsAndInt {
+    int x;
+    int y;
+    int val;
 };
 
 TEST_CASE("Testing array with ints.")
 {
-	utils::Array2D<int>
-		test_string{{{ 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }}};
+    utils::Array2D<int>
+            int_array{{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
 
-	SUBCASE("Test the values in the underlying vector match.")
-	{
-		std::vector<VectorsAndInt> position_tests{
-			{ 0, 0, 1 },
-			{ 1, 0, 2 },
-			{ 2, 0, 3 },
-			{ 3, 0, 4 },
-			{ 0, 1, 5 },
-			{ 1, 1, 6 },
-			{ 2, 1, 7 },
-			{ 3, 1, 8 },
-			{ 0, 2, 9 },
-			{ 1, 2, 10 },
-			{ 2, 2, 11 },
-			{ 3, 2, 12 },
-		};
+    SUBCASE("Test the values in the underlying vector match.") {
+        std::vector<VectorsAndInt> position_tests{
+                {0, 0, 1},
+                {1, 0, 2},
+                {2, 0, 3},
+                {3, 0, 4},
+                {0, 1, 5},
+                {1, 1, 6},
+                {2, 1, 7},
+                {3, 1, 8},
+                {0, 2, 9},
+                {1, 2, 10},
+                {2, 2, 11},
+                {3, 2, 12},
+        };
 
-		for (auto test : position_tests)
-		{
-			CHECK(test_string.at(test.x, test.y) == test.val);
-		}
-	}
+        for (auto test : position_tests) {
+            CHECK(int_array.at(test.x, test.y)==test.val);
+        }
+    }
 
-	SUBCASE("Test the sizes are all correct.")
-	{
-		SUBCASE("Test the underlying vector size.")
-		{
-			CHECK(test_string.size() == 12);
-		}
-		SUBCASE("Test the x size of the array.")
-		{
-			CHECK(test_string.get_size_x() == 4);
-		}
-		SUBCASE("Test the y size of the array.")
-		{
-			CHECK(test_string.get_size_y() == 3);
-		}
+    SUBCASE("Test the sizes are all correct.") {
+        SUBCASE("Test the underlying vector size.") {
+            CHECK(int_array.size()==12);
+        }
+        SUBCASE("Test the x size of the array.") {
+            CHECK(int_array.get_size_x()==4);
+        }
+        SUBCASE("Test the y size of the array.") {
+            CHECK(int_array.get_size_y()==3);
+        }
+    }
 
-	}
+}
+
+TEST_CASE("Testing equality operations with an array with ints.")
+{
+    utils::Array2D<int> int_array_org{{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
+    utils::Array2D<int> int_array_same{{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
+    utils::Array2D<int> int_array_different_size{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}};
+    utils::Array2D<int> int_array_different_values{{{4, 3, 2, 1}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
+
+    SUBCASE("Test equality operation.") {
+        SUBCASE("Arrays are equal.") {
+            CHECK(int_array_org == int_array_same);
+        }
+
+        SUBCASE("Arrays have different sizes.") {
+            CHECK(int_array_org != int_array_different_size);
+        }
+
+        SUBCASE("Arrays have different values.") {
+            CHECK(int_array_org != int_array_different_values);
+        }
+
+    }
+}
+
+
+TEST_CASE("Testing changing values in an array.")
+{
+    utils::Array2D<int>
+            int_array{{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
+
+    SUBCASE("Reassigning value in the array.") {
+        CHECK(int_array.at(1, 2)==10);
+        int_array.at(1, 2) = 20;
+        CHECK(int_array.at(1, 2)==20);
+    }
+
+    SUBCASE("Testing out inbuilt iterator.") {
+        CHECK(int_array.at(1, 2)==10);
+    }
 }
